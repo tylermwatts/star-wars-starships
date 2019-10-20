b8c5b0622a93ae7d6a925a4541d7f4d5002b761e
Warpfox <tylermwatts@gmail.com>
2019-10-20 10:27:06 -0500

    setup

---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import fetch from 'isomorphic-unfetch';
+import Link from 'next/link';
+import React from 'react';
+
+const StarshipPostLink = props => (
+  <li>
+    <Link href={`/starships/[sid]`} as={`/starships/${props.sid}`}>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a>{props.name}</a>
+    </Link>
+  </li>
+);
+
+const Index = props => (
+  <div>
+    <h1>Star Wars Starships</h1>
+    <ul>
+      {props.starships.map(s => (
+        <StarshipPostLink key={s} sid={s} name={s} />
+      ))}
+    </ul>
+  </div>
+);
+
+Index.getInitialProps = async () => {
+  const res = await fetch('https://swapi.co/api/starships/');
+  const data = await res.json();
+
+  console.log(`${data.results.length} starships found!`);
+
+  return {
+    starships: data.results.map(d => d.name)
+  };
+};
+
+export default Index;
---
+++ src/pages/starships/[sid].js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const StarShipPost = props => (
+  <div>
+    <h1>{props.ship.name}</h1>
+    <ul>
+      <li>Model: {props.ship.model}</li>
+      <li>Manufacturer: {props.ship.manufacturer}</li>
+      <li>Crew: {props.ship.crew}</li>
+      <li>Passengers: {props.ship.passengers}</li>
+      <li>Hyperdrive Rating: {props.ship.hyperdrive_rating}</li>
+      <li>Starship Class: {props.ship.starship_class}</li>
+      {props.ship.pilots.length > 0 && (
+        <li>{`Pilot(s): ${props.ship.pilots.join(', ')}`}</li>
+      )}
+    </ul>
+  </div>
+);
+
+StarShipPost.getInitialProps = async context => {
+  const { sid } = context.query;
+  const res = await fetch(`https://swapi.co/api/starships/?search=${sid}`);
+  const data = await res.json();
+
+  console.log(`Fetched ship: ${data.results[0].name}`);
+
+  return { ship: data.results[0] };
+};
+
+export default StarShipPost;
